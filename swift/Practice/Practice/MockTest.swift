389258bbab850744eb3c40e45d5e8671d64ba8f5
wiwi-git <duse34@hotmail.com>
2021-05-04 23:35:49 +0900

    라인수를 줄여봤다.

--- swift/Practice/Practice/MockTest.swift
+++ swift/Practice/Practice/MockTest.swift
@@ -10,36 +10,30 @@ class MockTest {
   struct TestPaper {
     let id:Int
     private let loopAnsers:[Int]
-    private var loopCount:Int {
-      didSet {
-        if loopCount == loopAnsers.count {
-          loopCount = 0
-        }
-      }
-    }
+    private var loopIndex:Int = 0
     var score = 0
     var answer: Int {
       mutating get {
-        let index = self.loopCount
-        self.loopCount += 1
+        let index = loopIndex
+        self.loopIndex += 1
+        if loopIndex >= loopAnsers.count { self.loopIndex = 0 }
         return self.loopAnsers[index]
       }
     }
     
-    init(id:Int,loopAnsers:[Int],_ loopCount:Int = 0) {
+    init(id:Int,loopAnsers:[Int]) {
       self.id = id
       self.loopAnsers = loopAnsers
-      self.loopCount = loopCount
     }
-    
   }
+  
   func solution(_ answers:[Int]) -> [Int] {
     var people:[TestPaper] = [
       TestPaper(id: 1, loopAnsers: [1,2,3,4,5]),
       TestPaper(id: 2, loopAnsers: [2,1,2,3,2,4,2,5]),
       TestPaper(id: 3, loopAnsers: [3,3,1,1,2,2,4,4,5,5])
     ]
-    
+    // calc score
     for answer in answers {
       for index in 0 ..< people.count {
         if answer == people[index].answer {
@@ -48,17 +42,13 @@ class MockTest {
       }
     }
     
-    people.sort { post, before in
-      post.score > before.score
-    }
+    // get top
+    let top = people.sorted(by: {$0.score > $1.score}).first!
     
-    var result = [people.first!.id]
-    
-    if people[0].score == people[1].score {
-      result.append(people[1].id)
-      if people[1].score == people[2].score {
-        result.append(people[2].id)
-      }
+    // get result
+    var result = [Int]()
+    for person in people.filter({ $0.score == top.score}) {
+      result.append(person.id)
     }
     return result
   }
--- swift/Practice/Practice/main.swift
+++ swift/Practice/Practice/main.swift
@@ -7,18 +7,5 @@
 
 import Foundation
 
-let truckP = TruckPassingBridge()
-struct Question {
-    let bridgeLength:Int
-    let weight:Int
-    let truckWeights:[Int]
-}
-var questions:[Question] = [
-    Question(bridgeLength: 2, weight: 10, truckWeights: [7,4,5,6]),
-//    Question(bridgeLength: 100, weight: 100, truckWeights: [10]),
-//    Question(bridgeLength: 100, weight: 100, truckWeights: [10,10,10,10,10,10,10,10,10,10])
-]
-
-for question in questions {
-    print(truckP.solution(question.bridgeLength, question.weight, question.truckWeights))
-}
+let mock = MockTest()
+print(mock.solution([1,2,3,4,5]))
